934e590b5bcd12a37edea48ce9fdf8a4bf416e89
angel-de-jesus <173237@ids.upchiapas.edu.mx>
2021-10-26 12:24:12 -0500

    v3

--- src/app/pages/carousel/carousel.module.ts
+++ src/app/pages/carousel/carousel.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { CarouselPageRoutingModule } from './carousel-routing.module';
+import {MatButtonModule} from '@angular/material/button';
 
 import { CarouselPage } from './carousel.page';
 
@@ -13,7 +14,8 @@ import { CarouselPage } from './carousel.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    CarouselPageRoutingModule
+    CarouselPageRoutingModule,
+    MatButtonModule,
   ],
   declarations: [CarouselPage]
 })
