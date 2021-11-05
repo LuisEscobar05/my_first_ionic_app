c9abaf3e3dfa48c18651bab122a93912ee21bf3b
LuisEscobar05 <181099@ids.upchiapas.edu.mx>
2021-11-05 14:33:01 -0600

    updates to deploy to heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+
+// Serve static files
+app.use(express.static(__dirname + '/dist/my_first_ionic_app'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/my_first_ionic_app/index.html'));
+});
+
+// default Heroku port
+app.listen(process.env.PORT || 8080);
