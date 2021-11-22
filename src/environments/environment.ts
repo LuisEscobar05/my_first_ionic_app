2b486f160f7ec61b4050ccc00850a243ad52534c
LuisEscobar05 <181099@ids.upchiapas.edu.mx>
2021-11-21 20:27:42 -0600

    add environment firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBfN8mwHeRnOGtY9vG1Btt6dr5P7-T_F6o",
+    authDomain: "barcode-eb236.firebaseapp.com",
+    projectId: "barcode-eb236",
+    storageBucket: "barcode-eb236.appspot.com",
+    messagingSenderId: "1091311168062",
+    appId: "1:1091311168062:web:d8c7ef0d891f71d1847f6f"
+  },
   production: false
 };
 
