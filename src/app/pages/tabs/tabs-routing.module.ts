39141f2fb25d3063f753208d26c5fb1c1e8d3f2e
LuisEscobar05 <181099@ids.upchiapas.edu.mx>
2021-11-21 20:26:44 -0600

    add method redirec

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -3,9 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { TabsPage } from './tabs.page';
 
+import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
+
+// Send unauthorized users to login
+const redirectUnauthorizedToLogin = () =>
+  redirectUnauthorizedTo(['/']);
+
+// // Automatically log in users
+// const redirectLoggedInToTabs = () => redirectLoggedInTo(['/tabs']);
+
+
 const routes: Routes = [
   {
     path:'',
+    ...canActivate(redirectUnauthorizedToLogin),
     redirectTo:'carousel',
   },
   {
@@ -14,18 +25,22 @@ const routes: Routes = [
     children:[
       {
         path:'carousel',
+        ...canActivate(redirectUnauthorizedToLogin),
         loadChildren: () => import('../carousel/carousel.module').then(m => m.CarouselPageModule)
       },
       {
         path:'update-user',
+        ...canActivate(redirectUnauthorizedToLogin),
         loadChildren: () => import('../update-user/update-user.module').then(m=>m.UpdateUserPageModule)
       },
       {
         path:'barcode',
+        ...canActivate(redirectUnauthorizedToLogin),
         loadChildren: () => import('../barcode/barcode.module').then(m => m.BarcodePageModule)
       },
       {
         path:'notifications',
+        ...canActivate(redirectUnauthorizedToLogin),
         loadChildren: () => import('../notifications/notifications.module').then(m => m.NotificationsPageModule)
       }
     ],
