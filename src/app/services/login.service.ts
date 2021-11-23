c8e21f1665155e1e3305a3b10f77cb1122b58a53
LuisEscobar05 <181099@ids.upchiapas.edu.mx>
2021-11-22 20:30:11 -0600

    add methods to verify email and reset password

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,36 +1,57 @@
 import { Injectable } from '@angular/core';
-import {switchMap, map,} from 'rxjs/operators';
-import { Observable, of } from 'rxjs';
-import {User} from '../models/user';
-import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth';
-import {Firestore, addDoc } from '@angular/fire/firestore';
-import { collection, onSnapshot, serverTimestamp, query, orderBy} from '@firebase/firestore';
-import { signInWithEmailAndPassword } from '@firebase/auth';
+import { Observable} from 'rxjs';
+import { User } from '../models/user';
+import { Auth, createUserWithEmailAndPassword,sendEmailVerification, signInWithEmailAndPassword,sendPasswordResetEmail } from '@angular/fire/auth';
+import { Firestore, docData, setDoc, doc, updateDoc, collection, where, onSnapshot } from '@angular/fire/firestore';
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
+  currentUser: any;
   constructor(
-    private auth:Auth,
-    private fs:Firestore,
+    private auth: Auth,
+    private fs: Firestore,
 
   ) { }
 
-  async signUp(user,email,password):Promise <any>{
-    const credential = await createUserWithEmailAndPassword(this.auth,email,password);
+  async signUp(user: User): Promise<any> {
+    const credential = await createUserWithEmailAndPassword(this.auth, user.email, user.password);
     const uid = credential.user.uid;
-    const userRef = collection(this.fs, 'users');
-
-    return addDoc(userRef,{uid,user,email});
-
+    const userRef = doc(this.fs, `users/${uid}`);
+    return setDoc(userRef, { username: user.username, email: user.email, name: '', ap_pat: '', ap_mat: '', numberPhone: '' });
   }
 
-  singIn(email,password){
-    return signInWithEmailAndPassword(this.auth, email, password);
+  singIn(user: User) {
+    return signInWithEmailAndPassword(this.auth, user.email, user.password);
   }
 
-  singOut(): Promise<void>{
+  singOut(): Promise<void> {
     return this.auth.signOut();
+  }
+
+  async update(user: User, id): Promise<any> {
+    const userRef = doc(this.fs, `users/${id}`);
+    return await updateDoc(userRef, { name: user.name, ap_pat: user.ap_pat, ap_mat: user.ap_mat, username: user.username, numberPhone: user.numberPhone })
+  }
+
+  getUserById(id): Observable<User> {
+    const userDocRef = doc(this.fs, `users/${id}`);
+    return docData(userDocRef, { idField: 'id' }) as Observable<User>
+  }
+
+  getUserLogged() {
+    return this.auth.currentUser.uid;
+  }
+
+  async sentResetPassword(email){
+    return await sendPasswordResetEmail(this.auth, email);
+  }
+
+  userVerifiedMail(){
+    return this.auth.currentUser.emailVerified;
+  }
+
+  async sendEmailToVerification(){
+    return await sendEmailVerification(this.auth.currentUser)
   }
 }
