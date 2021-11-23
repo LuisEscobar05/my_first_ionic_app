dabb84a0c22c327829d078797bebbf65fd161668
LuisEscobar05 <181099@ids.upchiapas.edu.mx>
2021-11-22 20:26:18 -0600

    update model User

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,6 +1,10 @@
 export interface User {
-  uid:string;
-  usuario:string;
-  correo:string;
-  nombre:string;
+  username:string;
+  name:string;
+  ap_pat:string;
+  ap_mat:string;
+  numberPhone:string;
+  password;
+  email;
+  repeatPassword;
 }
